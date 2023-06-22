cd60b896814317a062cd4557066e078d10eb1e4e
Bo Oppenheimer <booppenheimer@gmail.com>
2023-06-22 08:14:10 -0400

    no single quote

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default async function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Next v13.4 Todo's <br className="hidden sm:inline" />
+          Next v13.4 Todos <br className="hidden sm:inline" />
           built with Radix UI and Tailwind CSS.
         </h1>
       </div>
